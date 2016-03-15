b71da4cb7e99ae9563d901e821ff35801d1de877
LeoDeschamps <deschamps.leo07@gmail.com>
2016-03-15 16:50:18 +0100

    correction db

--- DB/Scripts/database.sql
+++ DB/Scripts/database.sql
@@ -1,4 +1,4 @@
-create database if not exists golden_tickets character set utf8 collate utf8_unicode_ci;
+create database if not exists golden_ticket character set utf8 collate utf8_unicode_ci;
 use golden_tickets;
 
 grant all privileges on golden_tickets.* to 'golden_tickets_user'@'localhost' identified by 'secret';
