f1e9a326c1501472c8d1299d79c8c5f7298f4ba3
YoannLth <yoann.lathuiliere@gmail.com>
2016-03-22 17:15:17 +0100

    Database modification + Database filling

---
+++ Code/pages/view.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link href="microcms.css" rel="stylesheet" />
+    <title>MicroCMS - Home</title>
+</head>
+<body>
+    <header>
+        <h1>MicroCMS</h1>
+    </header>
+    <?php foreach ($events as $event): ?>
+    <article>
+        <h2><?php echo $event['num_event'] ?></h2>
+        <p><?php echo $event['event_name'] ?></p>
+    </article>
+    <?php endforeach ?>
+    <footer class="footer">
+        <a href="https://github.com/bpesquet/OC-MicroCMS">MicroCMS</a> is a minimalistic CMS built as a showcase for modern PHP development.
+    </footer>
+</body>
+</html>
