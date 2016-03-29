4f53b922135b6f99b2c6cd3677a2b15bd9599c7f
YoannLth <yoann.lathuiliere@gmail.com>
2016-03-29 07:48:17 +0200

    Itération 7

--- Code/GoldenTicket/app/routes.php
+++ Code/GoldenTicket/app/routes.php
@@ -3,9 +3,5 @@
 // Home page
 $app->get('/', function () use ($app) {
     $events = $app['dao.event']->findAll();
-
-    ob_start();             // start buffering HTML output
-    require '../views/view.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
+    return $app['twig']->render('index.html.twig', array('events' => $events));
 });
